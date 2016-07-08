b7bde3279dc9d5a22e160c46b08afb797ebf7538
lucasborge <lucas.borge208@gmail.com>
2016-07-09 00:42:38 +0200

    change

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 
 $item_name = "";
 $sku = "";
+$image_url = "";
+$price = "";
+$open_date = "";
+$qty = "";
+$marketplace = "";
 if (isset($_GET['asin'])) {
     $asin = $_GET['asin'];
 
@@ -41,6 +46,15 @@ fclose ($ch);
         if ($asin == $asin1 || $asin == $asin2 || $asin == $asin3) {
             $item_name = $order['item-name'];
             $sku = $order['seller-sku'];
+            $desc = $order['item-description'];
+            $listing_id = $order['listing-id'];
+            $qty =$order['quantity'];
+            $open_date = $order['open-date'];
+            $image_url = $order['image-url'];
+            $marketplace = $order['item-is-marketplace'];
+            $type = $order['product-id-type'];
+            //zshop-shipping-fee,item-note,item-condition,zshop-category1,zshop-browse-path,zshop-storefront-feature,asin1,asin2,asin3,will-ship-internationally,expedited-shipping,zshop-boldface,product-id,bid-for-featured-placement,add-delete,pending-quantity,fulfillment-channel,business-price,"quantity price type","quantity lower bound 1","quantity price 1","quantity lower bound 2","quantity price 2","quantity lower bound 3","quantity price 3","quantity lower bound 4","quantity price 4","quantity lower bound 5","quantity price 5",merchant-shipping-group
+            $price = $order['price'];
             break;
         }
     }
@@ -71,11 +85,16 @@ fclose ($ch);
             <input type="button" value="search" onclick="get_asin()">
             <div id="result" style="border:1px solid #ddd;width:500px; height: 400px; overflow: auto;">
                 <?php
-                    echo "SKU : " . $sku;
+                    echo "SKU : " . $sku . "<br>";
+                    echo "PRICE : " . $price . "<br>";
+                    echo "QUANTITY : " . $qty . "<br>";
+                    echo "Open Date : " . $open_date . "<br>";
+                    echo "MarketPlace : " . $marketplace . "<br>";
                     echo "<br>";
                     echo "PRODUCT NAME : " . $item_name;
                 ?>
             </div>
+            <img src="<?php echo $image_url;?>" style="width:200px;">
         </form>
     </body>
 </html>
